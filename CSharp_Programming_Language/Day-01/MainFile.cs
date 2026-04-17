ffbdf555232aa388a53bd35d236865195e49b120
Aniket Kumar <aniketshraff@gmail.com>
2026-04-17 11:53:18 +0530

    Main file for Day One

---
+++ CSharp_Programming_Language/Day-01/MainFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CSharp_Programming_Language.Day_01
+{
+    internal class MainFile
+    {
+        public static void Main(string[] args)
+        {
+            DataTypes datatype = new DataTypes();
+
+            datatype.DType();
+
+            Operators opr = new Operators();
+            opr.OperatorsFun();
+        }
+    }
+}
