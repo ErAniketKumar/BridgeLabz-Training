e33793113946021cfb1405706b2bdb6d2824635e
Aniket Kumar <aniketshraff@gmail.com>
2026-04-22 17:36:02 +0530

    ladder - snake game

--- CSharp_Programming_Language/Programs.cs
+++ CSharp_Programming_Language/Programs.cs
@@ -1,6 +1,7 @@
 ﻿using CSharp_Programming_Language.Day_01;
 using CSharp_Programming_Language.Day_02;
 using CSharp_Programming_Language.Day_03;
+using CSharp_Programming_Language.Ladder_Snake_Game;
 
 
 //Operators oprator = new Operators();
@@ -9,20 +10,23 @@ using CSharp_Programming_Language.Day_03;
 
 
 //oprator.OperatorsFun();
-datatype.DType();
+//datatype.DType();
 
-ArrayMethod arraymethod = new ArrayMethod();
+//ArrayMethod arraymethod = new ArrayMethod();
 
-arraymethod.ArrayOperation();
+//arraymethod.ArrayOperation();
 
-StringOpr stringopr = new StringOpr();
+//StringOpr stringopr = new StringOpr();
 
-stringopr.StringMethod();
+//stringopr.StringMethod();
 
-MultiDimensionArray multiarray = new MultiDimensionArray();
+//MultiDimensionArray multiarray = new MultiDimensionArray();
 
-multiarray.MultiDArray();
+//multiarray.MultiDArray();
 
+GameScore gc = new GameScore();
+
+gc.GameLogic();
 
 
 
