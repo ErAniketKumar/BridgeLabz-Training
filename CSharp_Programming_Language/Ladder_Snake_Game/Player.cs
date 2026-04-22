f8733b56457a2526f009f2e7846d5327fcd4a033
Aniket Kumar <aniketshraff@gmail.com>
2026-04-22 17:35:38 +0530

    player information added

---
+++ CSharp_Programming_Language/Ladder_Snake_Game/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Programming_Language.Ladder_Snake_Game
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set;  }
+        public int Id { get; set; }
+        public Player(string name, int id)
+        {
+           this.Name = name;
+           this.Score = 0;
+           this.Id = id;
+        }
+
+        public int GetScore()
+        {
+            return this.Score;
+        }
+        public string GetPlayerName()
+        {
+            return this.Name;
+        }
+        public int GetPlayerId()
+        {
+            return this.Id;
+        }
+    }
+}
