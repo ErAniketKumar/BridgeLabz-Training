fe6350cedb487dfa4ab1b3efc6663247c5f9724a
Aniket Kumar <aniketshraff@gmail.com>
2026-04-25 12:48:08 +0530

    program added 6,7 called

--- ReviewFirst/Program.cs
+++ ReviewFirst/Program.cs
@@ -1,5 +1,10 @@
+using System.Drawing;
+using ReviewFIRST.AbstractClass;
 using ReviewFIRST.Basic;
+using ReviewFIRST.InterfaceProgram;
 
+
+// question one 
 SwapNumber swapNumber = new SwapNumber();
 
 int x;
@@ -17,6 +22,7 @@ for (int i = 0; i < list.Count; i++)
 
 
 
+// question two
 StringReverse strev = new StringReverse();
 
 string str = Console.ReadLine();
@@ -26,6 +32,9 @@ str = strev.ReverseString(str);
 System.Console.WriteLine(str);
 
 
+
+
+// question three
 ArmStrongNum armnum = new ArmStrongNum();
 
 int num;
@@ -41,15 +50,22 @@ else
 }
 
 
-
+// question 8
 Dice dice = new Dice();
 
 dice.DiceThrow();
 
+
+
+
+// question nine
 RandomEven randomEven = new RandomEven();
 
 randomEven.RandomEvenNumber();
 
+
+
+// question 10
 
 TwoNumLargeAndSmall largeSmall = new TwoNumLargeAndSmall();
 
@@ -61,7 +77,29 @@ bool ch2 = int.TryParse(Console.ReadLine(), out two);
 
 largeSmall.LargeSmall(one, two);
 
-
-
+// question 6
 AddDate adddate = new AddDate();
 adddate.DateAdd();
+
+
+
+// question 7
+DiffrenceDate diffrenceDate = new DiffrenceDate();
+TimeSpan ts = diffrenceDate.DateTimeDiff(DateTime.Now, (DateTime.Now.AddMonths(-6)));
+
+System.Console.WriteLine(ts);
+
+
+
+
+
+// question five
+
+ReviewFIRST.AbstractClass.Rectangle rc = new ReviewFIRST.AbstractClass.Rectangle(10, 20);
+
+Circle circle = new Circle(10);
+
+
+ContractEmployee ctemp = new ContractEmployee();
+
+FullTimeEmployee ftemp = new FullTimeEmployee();
