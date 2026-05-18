6be76ff364ac38a472fbae90cd49306111a98afb
ErAniketKumar <aniketshraff@gmail.com>
2026-05-18 15:45:20 +0530

    Linq practice

--- LINQ/Linq_Practice/LinqPractice.cs
+++ LINQ/Linq_Practice/LinqPractice.cs
@@ -111,17 +111,49 @@ public class LinqPractice
         };
 
         // sort by first paramater but when same then sort by second parameter descending
-        var data = list.OrderBy(b => b[0])
-        .ThenByDescending(b => b[1]);
+        // var data = list.OrderBy(b => b[0])
+        // .ThenByDescending(b => b[1]);
 
         // sort by acending if 1st paramater same then sort by 2nd paramater assending
-    //     var data = list.OrderBy(b => b[0])
-    //    .ThenBy(b => b[1]);
+        //     var data = list.OrderBy(b => b[0])
+        //    .ThenBy(b => b[1]);
 
-        foreach (var d in data)
+        // var data = list.GroupBy(l => l[0]);
+
+        // foreach (var d in data)
+        // {
+        //     Console.WriteLine($"Group Key = {d.Key}");
+
+        //     foreach (var item in d)
+        //     {
+        //         Console.WriteLine($"[{item[0]}, {item[1]}]");
+        //     }
+
+        //     Console.WriteLine();
+        // }
+
+
+        var data = list.GroupBy(ll => new
         {
-            System.Console.WriteLine(d[0] + " " + d[1]);
+            gkey = ll[0],
+            gcount = ll.Count
+        });
+
+        foreach (var keyVal in data)
+        {
+            Console.WriteLine(
+                $"Key = {keyVal.Key.gkey}, Count = {keyVal.Key.gcount}"
+            );
+
+            foreach (var val in keyVal)
+            {
+                Console.WriteLine($"[{val[0]}, {val[1]}]");
+            }
+
+            Console.WriteLine();
         }
+
+
     }
 
 
