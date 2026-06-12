37f785d8f7bb512ee7a1ca7bfbd51398dd6e34a0
ErAniketKumar <aniketshraff@gmail.com>
2026-06-12 17:06:30 +0530

    basic of reactive form

--- Angular/First-Tut/src/app/login/login.component.ts
+++ Angular/First-Tut/src/app/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  username = new FormControl();
+  password = new FormControl();
+
+
+  HandleLoginForm() {
+    console.log(this.username.value, this.password.value);
+  }
+
+
+  HandleSetDefaultValue() {
+    this.username.setValue("Aniket");
+    this.password.setValue("password123");
+  }
 
 }
