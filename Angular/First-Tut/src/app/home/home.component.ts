f3a81743d57958ea3585161d855a09bd38845975
ErAniketKumar <aniketshraff@gmail.com>
2026-06-12 11:20:51 +0530

    Declarative to gies power of element ngFor for iterative

--- Angular/First-Tut/src/app/home/home.component.ts
+++ Angular/First-Tut/src/app/home/home.component.ts
@@ -1,11 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  studentData = [
+    {name:"Aniket", age:20},
+    {name:"Kumar", age:21},
+    {name:"sonu", age:22},
+    {name:"soni", age:23},
+  ]
 }
