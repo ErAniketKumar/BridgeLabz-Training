984fa53f3b7d35b8feaae8089d3105a637c630bb
ErAniketKumar <aniketshraff@gmail.com>
2026-06-12 18:12:37 +0530

    form validation

--- Angular/First-Tut/src/app/login/login.component.ts
+++ Angular/First-Tut/src/app/login/login.component.ts
@@ -1,36 +1,59 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
   // username = new FormControl();
   // password = new FormControl();
-
 
   // HandleLoginForm() {
   //   console.log(this.username.value, this.password.value);
   // }
-
 
   // HandleSetDefaultValue() {
   //   this.username.setValue("Aniket");
   //   this.password.setValue("password123");
   // }
 
-    loginForm = new FormGroup({
-      username: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl()
-    })
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
-    HandleFormSubmit() {
-      console.log(this.loginForm.value);
-      console.log(this.loginForm.value.username);
+  HandleFormSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
     }
+    console.log(this.loginForm.value);
+    console.log(this.loginForm.value.username);
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
