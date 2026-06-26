81fea641c12ebf17be874ed803543aa9e1bdce2f
ErAniketKumar <aniketshraff@gmail.com>
2026-06-26 17:23:39 +0530

    revision d-1

---
+++ CSharp_Programming_Language/C#New-Series/Program.cs
@@ -0,0 +1,14 @@
+﻿decimal x = int.MaxValue;
+
+int y = 10;
+int z = 20;
+
+int res = checked(y + z);
+
+int d = int.MaxValue;
+
+int p = d+1;
+
+int q = checked(d+1);
+
+System.Console.WriteLine(q);;
