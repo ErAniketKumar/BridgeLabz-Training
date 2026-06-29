b58a0c6462457a33ad13c433dbef92f7f1f92af4
ErAniketKumar <aniketshraff@gmail.com>
2026-06-29 17:36:37 +0530

    singleton design create logger class

--- Design-Pattern-IMP/Creational/Singleton.cs
+++ Design-Pattern-IMP/Creational/Singleton.cs
@@ -4,12 +4,15 @@ using System.Text;
 
 namespace Design_Pattern_IMP.Creational
 {
+    // creating seald class for prevent inheritane with and avoid changing access specifier
     public sealed class Logger
     {
+        // static obj because we have to access in static method , static method only access static propery or variable
         private static Logger instance;
+        // lock - threading becuase multi threading they check thread one instace == null and same thread two then they create two or more instance 
         private static readonly object _lock = new object();
 
-
+        // private constructor becuase it prevent object creation outside 
         private Logger()
         {
 
--- Design-Pattern-IMP/Program.cs
+++ Design-Pattern-IMP/Program.cs
@@ -3,5 +3,7 @@ using Design_Pattern_IMP.Creational;
 
 Logger lg1 = Logger.GetInstance();
 Logger lg2 = Logger.GetInstance();
+Logger lg3 = Logger.GetInstance();
 
 Console.WriteLine(lg1==lg2);
+Console.WriteLine(lg1==lg2 && lg1==lg3);
