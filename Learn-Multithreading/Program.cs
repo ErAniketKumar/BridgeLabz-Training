c3779aa4370ccf4723102f1ada0638672528c330
ErAniketKumar <aniketshraff@gmail.com>
2026-06-29 11:28:06 +0530

    execute current method using thread i.e multiple method execute at same times

---
+++ Learn-Multithreading/Program.cs
@@ -0,0 +1,23 @@
+﻿void MultithreadingMethod1()
+{
+    for(int i =1;i<=100;i++)
+    {
+       Console.WriteLine("method 1st, id: "+ Thread.CurrentThread.ManagedThreadId);
+    }
+}
+
+
+
+void MultithreadingMethod2()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        Console.WriteLine("method 2nd, id: " + Thread.CurrentThread.ManagedThreadId);
+    }
+}
+
+Thread thread1 = new Thread(MultithreadingMethod1);
+thread1.Start();
+
+Thread thread2 = new Thread(MultithreadingMethod2);
+thread2.Start();
