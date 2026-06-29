79b1cf10913cd275181568a0afa2869b92e441c4
ErAniketKumar <aniketshraff@gmail.com>
2026-06-29 14:35:37 +0530

    race condition i.e multiple times random value

---
+++ Learn-Multithreading/M1/Example1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Learn_Multithreading.M1
+{
+    internal class Example1
+    {
+        static int count=0;
+        public void Increment()
+        {
+            //lock(this)
+            //{
+
+            for(int i=0;i<10000;i++)
+            {
+                count++;
+            }
+            Console.WriteLine(count + " thread name "+Thread.CurrentThread.Name);
+            }
+        //}
+    }
+}
--- Learn-Multithreading/Program.cs
+++ Learn-Multithreading/Program.cs
@@ -26,33 +26,88 @@
 
 //1 first
 
-void m1()
-{
-    Console.WriteLine(Thread.CurrentThread.Name);
-}
+//void m1()
+//{
+//    Console.WriteLine(Thread.CurrentThread.Name);
+//}
 
-Thread t1 = new Thread(m1);
-t1.Name = "Method 1";
+//Thread t1 = new Thread(m1);
+//t1.Name = "Method 1";
+
+//t1.Start();
+
+//// 2nd ways anomous method
+
+//Thread t2 = new Thread(() =>
+//{
+//    Console.WriteLine(Thread.CurrentThread.Name);
+//});
+
+//t2.Name = "method 2";
+
+//t2.Start();
+
+//// 3rd ways lamdba
+
+//new Thread(() =>
+//{
+//    Console.WriteLine("Method 3");
+//}).Start();
+
+
+
+
+
+//int start = int.Parse(Console.ReadLine());
+//int end =  int.Parse(Console.ReadLine());
+
+//void printSum(int start, int end)
+//{
+//    int sum = 0;
+//    for(int i= start;i<=end;i++)
+//    {
+//        sum += i;
+//    }
+
+//    Console.WriteLine(sum);
+//}
+
+//void printSumAvd(int start,int end)
+//{
+//    int sum = 0;
+//    int count = 0;
+//    for (int i = start; i <= end; i++)
+//    {
+//        sum += i;
+//        count++;
+//    }
+
+//    Console.WriteLine(sum / count);
+//}
+
+//Thread t1 = new Thread(()=> printSum(start, end));
+//t1.Start();
+
+//Thread t2 = new Thread(()=> printSumAvd(start, end));
+
+//t2.Start();
+
+//t1.Join();
+//t2.Join();
+
+
+using Learn_Multithreading.M1;
+
+Example1 ex1 = new Example1();
+
+Thread t1 = new Thread(ex1.Increment);
+
+Thread t2 = new Thread(ex1.Increment);
+
+t1.Name = "thread 1";
+t2.Name = "thread 2";
 
 t1.Start();
-
-// 2nd ways anomous method
-
-Thread t2 = new Thread(() =>
-{
-    Console.WriteLine(Thread.CurrentThread.Name);
-});
-
-t2.Name = "method 2";
-
 t2.Start();
 
-// 3rd ways lamdba
 
-new Thread(() =>
-{
-    Console.WriteLine("Method 3");
-}).Start();
-
-
- 
