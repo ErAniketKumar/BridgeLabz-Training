b8ed2ec1fede5668232e4a96c6c3ae368dd60f04
ErAniketKumar <aniketshraff@gmail.com>
2026-06-30 18:33:52 +0530

    todolist

--- jQuery/js/todo.js
+++ jQuery/js/todo.js
@@ -1,36 +1,104 @@
 jQuery.noConflict();
 
-jQuery(document).ready(($) => {
+jQuery(document).ready(function ($) {
   let tasks = [];
-  let idx = 0;
-  let taskHtml = "";
+  let idx = 1;
 
+  // Show all tasks
+  function renderTasks() {
+    let html = "";
+
+    tasks.forEach(function (task) {
+      html += `
+                <div class="task" data-id="${task.id}">
+
+                    <div>
+
+                        <input
+                            type="checkbox"
+                            class="check-task"
+                            ${task.completed ? "checked" : ""}
+                        >
+
+                        <span class="${task.completed ? "completed" : ""}">
+                            ${task.title}
+                        </span>
+
+                    </div>
+
+                    <div>
+                        <button class="edit-btn">Edit</button>
+                        <button class="delete-btn">Delete</button>
+                    </div>
+
+                </div>
+            `;
+    });
+
+    $("#show-result").html(html);
+  }
+
+  // Add Task
   $("#todo-form").submit(function (e) {
     e.preventDefault();
 
-    let data = $("#task").val();
+    let title = $("#task").val().trim();
+
+    if (title === "") return;
+
+    tasks.push({
+      id: idx++,
+      title: title,
+      completed: false,
+    });
+
     $("#task").val("");
 
-    let obj = {
-      title: data,
-      id: idx++,
-      status: "pending",
-    };
-    tasks.push(obj);
-
-    taskHtml = `
-          <div style="display:flex">
-          <span> ${obj["title"]}</span>
-          <input type="checkbox">
-          <div>
-          <button>edit</button>
-          <button>del</button>
-          </div>
-          </div>
-          `;
-    $("#show-result").append(taskHtml);
+    renderTasks();
   });
 
-  
+  // Delete Task
+  $("#show-result").on("click", ".delete-btn", function () {
+    let id = $(this).closest(".task").data("id");
 
+    tasks = tasks.filter(function (task) {
+      return task.id != id;
+    });
+
+    renderTasks();
+  });
+
+  // Edit Task
+  $("#show-result").on("click", ".edit-btn", function () {
+    let id = $(this).closest(".task").data("id");
+
+    let task = tasks.find(function (task) {
+      return task.id == id;
+    });
+
+    let newTitle = prompt("Edit Task", task.title);
+
+    if (newTitle === null) return;
+
+    newTitle = newTitle.trim();
+
+    if (newTitle === "") return;
+
+    task.title = newTitle;
+
+    renderTasks();
+  });
+
+  // Complete Task
+  $("#show-result").on("change", ".check-task", function () {
+    let id = $(this).closest(".task").data("id");
+
+    let task = tasks.find(function (task) {
+      return task.id == id;
+    });
+
+    task.completed = this.checked;
+
+    renderTasks();
+  });
 });
