0d416133aab3c41a5a367d13a847ee0b3bd9e82c
ErAniketKumar <aniketshraff@gmail.com>
2026-07-05 18:54:19 +0530

    passing data parent to child

--- F-1-Angular-Learning-with-testing/First/src/app/app.ts
+++ F-1-Angular-Learning-with-testing/First/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Todo } from './todo/todo';
 import { Getset } from './getset/getset';
 import { Todolist } from './todolist/todolist';
+import { Parent } from './parent/parent';
 
 @Component({
   selector: 'app-root',
-  imports: [Todo, Getset, Todolist],
+  imports: [Todo, Getset, Todolist, Parent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ F-1-Angular-Learning-with-testing/First/src/app/parent/parent.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { Child } from '../child/child';
+
+@Component({
+  selector: 'app-parent',
+  imports: [Child],
+  templateUrl: './parent.html',
+  styleUrl: './parent.css',
+})
+export class Parent {
+  flower = signal('rose');
+
+  users = signal([
+    { id: 1, name: 'Aniket', age: 20 },
+    { id: 2, name: 'Kumar', age: 22 },
+    { id: 3, name: 'Abhishek', age: 19 },
+  ]);
+  
+}
