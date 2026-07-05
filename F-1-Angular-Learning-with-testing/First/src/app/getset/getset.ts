d4e0e358e986cd10e2cba30f59f2327882a6db9c
ErAniketKumar <aniketshraff@gmail.com>
2026-07-05 10:57:39 +0530

    looping and conditioanl and switching

--- F-1-Angular-Learning-with-testing/First/src/app/getset/getset.ts
+++ F-1-Angular-Learning-with-testing/First/src/app/getset/getset.ts
@@ -15,4 +15,29 @@ export class Getset {
   setValue(val: string) {
     this.value.set(val);
   }
+
+  data = signal([
+    { id: 1, name: 'Aniket', age: 21 },
+    {
+      id: 2,
+      name: 'kumar',
+      age: 22,
+    },
+    {
+      id: 3,
+      name: 'Rahul',
+      age: 20,
+    },
+    {
+      id: 4,
+      name: 'Sumit',
+      age: 23,
+    },
+  ]);
+
+  day = signal(0);
+
+  showDay(day: string) {
+    this.day.set(Number(day));
+  }
 }
