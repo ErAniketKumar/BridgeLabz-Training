e3b13b9172a2bf871c5ad3f7aef05aa9aec3faa6
ErAniketKumar <aniketshraff@gmail.com>
2026-07-06 19:48:06 +0530

    signal form

--- F-1-Angular-Learning-with-testing/First/src/app/form/register/register.ts
+++ F-1-Angular-Learning-with-testing/First/src/app/form/register/register.ts
@@ -1,15 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { form, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, FormField],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  regiterFormHandler(data: NgForm) {
-    console.log(data);
+  // regiterFormHandler(data: NgForm) {
+  //   console.log(data);
+  // }
+
+  loginFormModel = signal({
+    email: '',
+    password: '',
+  });
+
+  loginForm = form(this.loginFormModel);
+  
+  submitHandler() {
+    console.log(this.loginForm.email().value());
+    console.log(this.loginForm.password().value());
   }
 }
